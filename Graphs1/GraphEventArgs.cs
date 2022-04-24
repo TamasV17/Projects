5e4b5150f5e06261d535b944d2c7050795dedc1d
Tamás Varga <99874389+TamasV17@users.noreply.github.com>
2022-04-24 14:00:44 +0200

    Add files via upload

---
+++ Graphs1/GraphEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs1
+{
+    class GraphEventArgs<T>:EventArgs
+    {
+        public GraphEventArgs(T from, T to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public T From { get; set; }
+        public T To { get; set; }
+    }
+}
---
+++ Graphs1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graphs1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Graph<Person> graph = new Graph<Person>();
+            Person Stew = new Person("Stew");
+            graph.AddNode(Stew);
+            Person Joseph = new Person("Joseph");
+            graph.AddNode(Joseph);
+            Person Marge = new Person("Marge");
+            graph.AddNode(Marge);
+            Person Gerald = new Person("Gerald");
+            graph.AddNode(Gerald);
+            Person Zack = new Person("Zack");
+            graph.AddNode(Zack);
+            Person Janet = new Person("Janet");
+            graph.AddNode(Janet);
+            Person Peter = new Person("Peter");
+            graph.AddNode(Peter);
+            graph.AddEdge(graph.contents[graph.contents.IndexOf(Stew)], graph.contents[graph.contents.IndexOf(Marge)], EdgeDisplay);
+            graph.AddEdge(graph.contents[graph.contents.IndexOf(Stew)], graph.contents[graph.contents.IndexOf(Joseph)], EdgeDisplay);
+            graph.AddEdge(graph.contents[graph.contents.IndexOf(Marge)], graph.contents[graph.contents.IndexOf(Joseph)], EdgeDisplay);
+            graph.AddEdge(graph.contents[graph.contents.IndexOf(Joseph)], graph.contents[graph.contents.IndexOf(Gerald)], EdgeDisplay);
+            graph.AddEdge(graph.contents[graph.contents.IndexOf(Joseph)], graph.contents[graph.contents.IndexOf(Zack)], EdgeDisplay);
+            graph.AddEdge(graph.contents[graph.contents.IndexOf(Zack)], graph.contents[graph.contents.IndexOf(Gerald)], EdgeDisplay);
+            graph.AddEdge(graph.contents[graph.contents.IndexOf(Zack)], graph.contents[graph.contents.IndexOf(Peter)], EdgeDisplay);
+            graph.AddEdge(graph.contents[graph.contents.IndexOf(Peter)], graph.contents[graph.contents.IndexOf(Janet)], EdgeDisplay);
+            Console.WriteLine("BFS test:");
+            graph.BFS(Janet, Show);
+            Console.WriteLine("DFS test:");
+            graph.DFS_starter(Zack, Show);
+
+        }
+        static void EdgeDisplay(object source, GraphEventArgs<Person> geargs)
+        {
+            Console.WriteLine($"Edge between {geargs.From} and {geargs.To}");
+        }
+        static void CounterDisplay(string From, string To, int r_Counter)
+        {
+            Console.WriteLine($"{From} and {To} {r_Counter}-nd/st/th neighbors.");
+        }
+        public static void Show(string content)
+        {
+            Console.WriteLine(content);
+        }
+    }
+}
